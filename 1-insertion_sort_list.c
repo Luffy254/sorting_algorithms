67b47244cbd5cf8573a26b3f010705c91fc0ad3b
Samuel Kiarie <skiarie21@gmail.com>
2023-09-26 09:51:49 -0700

    update 3

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,41 +1,33 @@
 #include "sort.h"
 /**
  * swap_nodes - Swaps two nodes in a doubly linked list
- * @list: head of list@list: Pointer to the head of the list
+ * @head: head of list@list: Pointer to the head of the list
  * @node1: First node to swap
  * @node2: Second node to swap
  */
 
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+void swap_nodes(listint_t **head, listint_t *node1, listint_t *node2)
 {
-	listint_t *prev1, *next1, *prev2, *next2;
+	listint_t *auxy1, *auxy2;
 
 	if (node1 == NULL || node2 == NULL)
 		return;
 
-	prev1 = node1->prev;
-	next1 = node1->next;
-	prev2 = node2->prev;
-	next2 = node2->next;
+	auxy1 = node1->prev;
+	auxy2 = node2->next;
 
-	if (prev1)
-		prev1->next = node2;
-	if (next1)
-		next1->prev = node2;
-	if (prev2)
-		prev2->next = node1;
-	if (next2)
-		next2->prev = node1;
+	if (auxy1)
+		auxy1->next = node2;
+	if (auxy2)
+		auxy2->prev = node1;
 
-	node1->next = next2;
-	node1->prev = prev2;
-	node2->next = next1;
-	node2->prev = prev1;
+	node1->next = auxy2;
+	node1->prev = node2;
+	node2->next = node1;
+	node2->prev = auxy1;
 
-	if (prev1 == NULL)
-		*list = node2;
-	if (prev2 == NULL)
-		*list = node1;
+	if (auxy1 == NULL)
+		*head = node2;
 }
 
 /**
@@ -44,20 +36,21 @@ void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
  */
 void insertion_sort_list(listint_t **list)
 {
-	if (list == NULL || *list == NULL)
+	listint_t *curr, *prev;
+	int val;
+
+	if (list == NULL || (*list)->next == NULL || *list == NULL)
 		return;
 
-	listint_t *curr, *prev;
-
-	curr = (*list)->next;
-
-	while (curr != NULL)
+	curr = *list;
+	while (curr)
 	{
 		prev = curr->prev;
+				val = curr->n;
 
-		while (prev != NULL && prev->n > curr->n)
+		while (prev && prev->n > val)
 		{
-			swap_nodes(list, prev, curr);
+			swap_nodes(prev, curr, list);
 			print_list(*list);
 			prev = curr->prev;
 		}
